89023565c664c2ace8c4125d8147f909b0ecf456
elysalem-missawi <elysalemmissawi@gmail.com>
2026-09-05 14:17:56 +0200

    feat: add project and article content types

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,16 @@
-import type { Core } from '@strapi/strapi';
-
-const config: Core.Config.Middlewares = [
+ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
@@ -12,5 +18,3 @@ const config: Core.Config.Middlewares = [
   'strapi::favicon',
   'strapi::public',
 ];
-
-export default config;
